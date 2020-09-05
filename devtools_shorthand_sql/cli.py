686fd622170b55e91c6d6022447d4dc64b464043
HaeckelK <noneofyour@biz.com>
2020-09-05 14:15:02 +0100

    feat: sql_type to cli

--- devtools_shorthand_sql/cli.py
+++ devtools_shorthand_sql/cli.py
@@ -2,17 +2,17 @@
 import argparse
 import sys
 
+import devtools_shorthand_sql.core as core
+
 
 def main():
-    """Console script for devtools_shorthand_sql."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('filename', type=str, help='Full path to shorthand file.')
+    parser.add_argument('--sql_type', choices=['sqlite', 'postgres'],
+                        default='sqlite',
+                        help='RDMS style to be used for produced statements.')
     args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "devtools_shorthand_sql.cli.main")
-    return 0
+    core.main(args.filename, args.sql_type)
 
 
 if __name__ == "__main__":
