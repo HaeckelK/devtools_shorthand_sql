fbcb1f1c6c987f6548e50153e923a77c24383825
HaeckelK <noneofyour@biz.com>
2020-09-06 14:40:45 +0100

    feat: add pytest output

--- devtools_shorthand_sql/core.py
+++ devtools_shorthand_sql/core.py
@@ -10,10 +10,17 @@ def load_instructions_file(filename: str) -> str:
 
 
 class Field():
+    test_default = ''
     def __init__(self, name, field_type):
         self.name = name
         self.field_type = field_type
         return
+
+    @property
+    def kwarg(self):
+        if isinstance(self.test_default, str):
+            return '"' + self.test_default + '"'
+        return self.test_default
 
     def lowercase(self):
         self.name = self.name.lower()
@@ -22,6 +29,27 @@ class Field():
     def uppercase(self):
         self.name = self.name.upper()
         return
+
+
+class IntegerField(Field):
+    test_default = 999
+
+
+class RealField(Field):
+    test_default = 3.5
+
+
+class TextField(Field):
+    test_default = '123fakestreet'
+
+
+# TODO implement blob test
+class BlobField(Field):
+    pass
+
+
+class IDField(Field):
+    test_default = 1
 
 
 class SQLBuilder():
@@ -52,6 +80,22 @@ class SQLBuilder():
         definition = f'def {function_name}({arguments}):'
         return sql, params, definition, function_name
 
+    def create_test(self, function_name):
+        expected = tuple([field.test_default for field in self.fields])
+        kwargs = []
+        for field in self.fields:
+            if isinstance(field, IDField):
+                continue
+            kwarg = field.name + '=' + str(field.kwarg)
+            kwargs.append(kwarg)
+        arguments = ', '.join(kwargs)
+        sql = f"'SELECT * FROM {self.table_name}'"
+        code = f"""def test_{function_name}():\n\
+    expected = {expected}
+    YOUR_MODULE.{function_name}({arguments})\n\
+    result = YOUR_CONNECTOR_QUERY({sql}).fetchall()[0]\n    assert result == expected"""
+        return code
+
 
 class PostgresSQLBuilder(SQLBuilder):
     value_char = '%s' 
@@ -76,11 +120,16 @@ def main(filename: str, sql_type: str):
             continue
         table_name = lines[0].replace('table', '').strip()
         raw_fields = [x.split(' ') for x in lines[1:]]
-        fields = [Field('id', 'INTEGER PRIMARY KEY')]
+        fields = [IDField('id', 'INTEGER PRIMARY KEY')]
         for raw_field in raw_fields:
             if len(raw_field) != 2:
                 continue
-            field = Field(raw_field[0], raw_field[1])
+            field_type = raw_field[1]
+            field = Field(raw_field[0], field_type)
+            if field_type == 'int':
+                field = IntegerField(raw_field[0], field_type)
+            if field_type == 'text':
+                field = TextField(raw_field[0], field_type)
             fields.append(field)
         for field in fields:
             field.lowercase()
@@ -92,10 +141,14 @@ def main(filename: str, sql_type: str):
 
         table_sql = builder.create_table_statement()
         insert_sql, params, definition, function_name = builder.create_insert_statement()
+        test_function = builder.create_test(function_name)
+
         print('\n')
         print(table_sql)
         print('\n')
         print(definition)
         print(insert_sql)
         print(params)
+        print('\n')
+        print(test_function)
     return
