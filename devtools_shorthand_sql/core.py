7a8e387f979a9dbf3651506d0ff6a7b07d45bbff
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2020-09-06 15:51:15 +0100

    refactor: move Field classes into own module (#6)

--- devtools_shorthand_sql/core.py
+++ devtools_shorthand_sql/core.py
@@ -2,54 +2,13 @@
 import argparse
 from typing import List
 
+from devtools_shorthand_sql.fields import Field, BlobField, IDField, IntegerField, RealField, TextField
+
 
 def load_instructions_file(filename: str) -> str:
     with open(filename, 'r') as f:
         contents = f.read()
     return contents
-
-
-class Field():
-    test_default = ''
-    def __init__(self, name, field_type):
-        self.name = name
-        self.field_type = field_type
-        return
-
-    @property
-    def kwarg(self):
-        if isinstance(self.test_default, str):
-            return '"' + self.test_default + '"'
-        return self.test_default
-
-    def lowercase(self):
-        self.name = self.name.lower()
-        return
-
-    def uppercase(self):
-        self.name = self.name.upper()
-        return
-
-
-class IntegerField(Field):
-    test_default = 999
-
-
-class RealField(Field):
-    test_default = 3.5
-
-
-class TextField(Field):
-    test_default = '123fakestreet'
-
-
-# TODO implement blob test
-class BlobField(Field):
-    pass
-
-
-class IDField(Field):
-    test_default = 1
 
 
 class SQLBuilder():
