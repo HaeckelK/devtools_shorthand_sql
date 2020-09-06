af1902e54a7f86aceef25b065308e604eeb6b743
HaeckelK <noneofyour@biz.com>
2020-09-06 18:20:06 +0100

    Bump version: 0.2.1 → 0.3.0

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/HaeckelK/devtools_shorthand_sql',
-    version='0.2.1',
+    version='0.3.0',
     zip_safe=False,
 )
