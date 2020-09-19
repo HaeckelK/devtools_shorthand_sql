5006a4669a994c9898d589004414b8a9e7e6d598
HaeckelK <noneofyour@biz.com>
2020-09-19 15:57:55 +0100

    tests: enough tests to begin importer refactor

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -17,6 +17,8 @@ def test_map_raw_field_data_type():
 
 
 def test_parse_instructions_into_x():
+    # This is just a high level test to check that the final output
+    # still works after recfactoring.
     content = """# table photo
 ID id
 SIZE int
@@ -24,3 +26,9 @@ FILENAME text
 date_taken int
 is_DELeted boolean"""
     result = parser.parse_instructions_into_x(content)
+    fields = result[0]['fields']
+    assert result[0]['table_name'] == 'photo'
+    assert fields[0].field_type == 'INTEGER PRIMARY KEY'
+    assert fields[1].field_type == 'INT'
+    assert fields[2].field_type == 'TEXT'
+    assert fields[3].field_type == 'INT'
