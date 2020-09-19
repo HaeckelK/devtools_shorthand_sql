26ba6f56063068f9fe24fe5b83e873a0aeef0432
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2020-09-19 19:59:24 +0100

    Import parser (#20)
    * tests: refactor
    
    * tests: additional error handling cases

--- devtools_shorthand_sql/utils.py
+++ devtools_shorthand_sql/utils.py
@@ -1,6 +1,17 @@
+"""
+
+fatal_error: Used when user input cannot be processed and program must terminate.
+
+info_message: Print message to user that is informational in content only and does not indicate an error.
+
+"""
 import sys
 
 
 def fatal_error(message):
     print('Error: ' + message)
     sys.exit()
+
+
+def info_message(message):
+    print('Info: ' + message)
