0b162709ca90ff63032f7f56c06efd2cb320d063
HaeckelK <noneofyour@biz.com>
2020-09-19 15:19:30 +0100

    tests: sqlbuilder properties

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 import pytest
 
+import random
+
 from devtools_shorthand_sql import core
 
+random.seed(1234)
 
 def test_map_raw_field_data_type():
     # field type exists, upper
@@ -22,3 +25,15 @@ def test_base_function():
     assert base.name == name
     assert base.text == text
     assert base.__str__() == text
+
+
+def test_sql_builder():
+    fields = [core.IntegerField('col1', 'test'), core.TextField('COL2', 'test2')]
+    x = core.SQLBuilder('my_table', fields)
+    assert x.arguments == 'col1: int, col2: str'
+    assert x.field_names == 'col1, COL2'
+    assert x.params == 'col1, col2'
+    assert x.values == '?,?'
+    assert x.function_name_stem == 'my_table'
+    assert x.has_idfield is False
+    assert x.kwargs == 'col1=902, col2="ED73BYDMA9"'
