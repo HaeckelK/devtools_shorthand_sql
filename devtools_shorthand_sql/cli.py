0390a6420874febdd6d28ea96ab9f960b3e94b32
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2020-09-20 13:07:28 +0100

    Output report (#25)
    * feat: basic saving to text file
    
    * feat: add output_filename cli argument
    
    * feat: change # to ## for sub sections

--- devtools_shorthand_sql/cli.py
+++ devtools_shorthand_sql/cli.py
@@ -8,11 +8,13 @@ import devtools_shorthand_sql.core as core
 def main():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('filename', type=str, help='Full path to shorthand file.')
+    parser.add_argument('-o', '--output_filename', type=str, help='Full filename including path for where to save created \
+        functions', default='shorthand_sql_created_functions.txt')
     parser.add_argument('--sql_type', choices=['sqlite', 'postgres'],
                         default='sqlite',
                         help='RDMS style to be used for produced statements.')
     args = parser.parse_args()
-    core.main(args.filename, args.sql_type)
+    core.main(args.filename, args.sql_type, args.output_filename)
 
 
 if __name__ == "__main__":
